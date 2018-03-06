096e5350e2a401e331fc45a8ffd6006884f7fcc3
aaron k saunders <aaron@clearlyinnovative.com>
2018-03-06 15:23:02 -0500

    saving info for auto login

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import Vue from 'nativescript-vue';
+import Vue from "nativescript-vue";
 
-import router from './router';
+import router from "./router";
 
-import store from './store';
+import store from "./store";
 
-import './styles.scss';
+import "./styles.scss";
 
 // Uncommment the following to see NativeScript-Vue output logs
 Vue.config.silent = false;
 
-console.log(store.state)
-if ( store.state.auth.isLoggedIn ) {
-  router.replace("/home");
-} else {
-  router.replace("/login");
-}
+console.log(store.state);
+store.dispatch("auth/init").then(user => {
+  if (user) {
+    router.replace("/home");
+  } else {
+    router.replace("/login");
+  }
+});
 
 new Vue({
-
   router,
 
-  store,
-
+  store
 }).$start();
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,3 +1,5 @@
+const appSettings = require("tns-core-modules/application-settings");
+
 const state = {
   loggedIn: false,
   user: null
@@ -5,22 +7,56 @@ const state = {
 
 const mutations = {
   logout(state) {
-    console.log("logging out ", user)
+    console.log("logging out ", user);
     state.loggedIn = false;
     state.user = null;
+
+    // remove the user email if we are logged out
+    appSettings.removeString("AUTH-INFO");
   },
   login(state, { user }) {
-    console.log("logging in ", user)
+    console.log("logging in ", user);
     state.loggedIn = true;
     state.user = user;
+
+    // save the user email if we are logged in
+    appSettings.setString("AUTH-INFO", user.email);
   }
 };
 
 const actions = {
+  /**
+   *
+   */
   logout: ({ commit }) => commit("logout"),
+  /**
+   *
+   */
   login: ({ commit }, user) => {
-    commit("login", { user })
-    return Promise.resolve({...user})
+    commit("login", { user });
+
+    // return the logged in user information
+    return Promise.resolve({ ...user });
+  },
+  /**
+   *
+   */
+  init: ({ commit }) => {
+    // check setting for auth information
+    let authString = appSettings.getString("AUTH-INFO");
+
+    // if i have data then do login, else just return
+    // with no user information...
+    if (authString) {
+      console.log("found saved user...", authString)
+      let user = { email: authString };
+      commit("login", { user });
+      // return the logged in user information
+      return Promise.resolve({ ...user });
+    } else {
+      console.log("NO saved user...")
+      return Promise.resolve();
+    }
   }
 };
 
